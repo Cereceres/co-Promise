deafc7b53d0b1091defcd77b5ec8720c74be922a
cereceres <jesus.edelcereceres@gmail.com>
2018-09-03 18:52:00 -0600

    the node_modules is added to gitignore

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 const co = require('co')
-
-class CoPromise{
-  constructor(generator, ctx) {
+var isGenerator = require('is-generator')
+var isGeneratorFn = require('is-generator').fn
+class CoPromise {
+  constructor (generator, ctx) {
     this.generator = generator
     this.ctx = ctx
-    var _this = this
     this.promise = new Promise((resolve, reject) => co
-      .call(ctx || this, _this.generator, resolve, reject))
+      .call(ctx || this, generator, resolve, reject))
   }
 
-    then(resHandler, rejHandler) {
-      this.promise= this.promise.then((res)=>co.call(this, resHandler, res))
-      if (rejHandler) this.promise = this.promise.catch(err => co.call(this,rejHandler, err))
-
-     return this
-    }
-    catch(rejHandler) {
-      this.promise = this.promise.catch(err => co.call(this,rejHandler, err))
-
-     return this
-    }
-
-    static all(arrayPromises){
-      return new CoPromise(function*(resolve, reject){
-        const res = yield Promise.all(arrayPromises).catch(reject)
-        resolve(res)
-      }, this)
+  then (resHandler, rejHandler) {
+    if (isGenerator(resHandler) || isGeneratorFn(resHandler)) {
+      this.promise = this
+        .promise.then((res) => co.call(this, resHandler, res))
+    } else if (isGenerator(rejHandler) || isGeneratorFn(rejHandler)) {
+      this.promise = this
+        .promise.catch(err => co.call(this, rejHandler, err))
+    } else {
+      this.promise = this
+        .promise.then(resHandler, rejHandler)
     }
 
-    static reject(error){
-      return new CoPromise(function*(resolve, reject){return reject(error)})
+    return this
+  }
+  catch (rejHandler) {
+    if (isGenerator(rejHandler) || isGeneratorFn(rejHandler)) {
+      this.promise = this
+        .promise.catch(err => {
+          return co.call(this, rejHandler, err)
+        })
+    } else {
+      this.promise = this.promise.catch(err => {
+        return rejHandler(err)
+      })
     }
 
-    static resolve(res){
-      return new CoPromise(function*(resolve){return resolve(res)})
-    }
+    return this
+  }
 
+  static all (arrayPromises) {
+    return new CoPromise(function * (resolve, reject) {
+      const res = yield Promise.all(arrayPromises).catch(reject)
+      resolve(res)
+    }, this)
+  }
+
+  static reject (error) {
+    return new CoPromise(function * (resolve, reject) { return reject(error) })
+  }
+
+  static resolve (res) {
+    return new CoPromise(function * (resolve) { return resolve(res) })
+  }
 }
 
 module.exports = CoPromise
--- test/test.js
+++ test/test.js
@@ -2,14 +2,14 @@
 let assert = require('assert')
 let CoPromise = require('../index')
 
-describe('Test for CoPromise', function() {
-  before(function() {
-    //the generator are called with a arg and next, what is the next generator
-    this.gen1 = function*(resolve, reject) {
+describe('Test for CoPromise', function () {
+  before(function () {
+    // the generator are called with a arg and next, what is the next generator
+    this.gen1 = function * (resolve, reject) {
       // every generator is wrapper with co
       let res = yield Promise.resolve(4)
       assert.equal(res, 4)
-        //this promise is resolved with 6
+      // this promise is resolved with 6
       let dist = Math.random()
       if (dist > 0.5) {
         resolve(dist)
@@ -20,16 +20,33 @@ describe('Test for CoPromise', function() {
       }
     }
   })
-  it('should resolve the promise', function(done) {
+  it('should resolve the promise', function (done) {
     (new CoPromise(this.gen1))
-    .then(function*(res) {
+      .then(function * (res) {
         let _res = yield Promise.resolve(res)
         assert(_res > 0.5) // true
         done()
       })
-      .catch(function*(_error) {
+      .catch(function * (_error) {
         let error = yield Promise.resolve(_error)
         assert(error === 'hola' || error < 0.2) // true
+        done()
+      })
+  })
+
+  it('should resolve the promise', function (done) {
+    new CoPromise(function * (resolve) {
+      const p = new CoPromise(function * (resolve) {
+        const res = yield Promise.resolve(2)
+        assert(res === 2)
+        resolve(res * 3)
+      })
+      const res = yield p
+      resolve(res)
+    })
+      .then(function * (res) {
+        let _res = yield Promise.resolve(res)
+        assert(_res === 6) // true
         done()
       })
   })
